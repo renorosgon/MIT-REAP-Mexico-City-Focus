d4b87e40b22fc1fd839f2b593f182bfba3a4d8b9
René Rosado González <42519434+renorosgon@users.noreply.github.com>
2023-04-06 15:36:02 -0600

    Finally figured out wth with spark and tidymodels
    Also minor corrections

--- R/1-IcapDataPrep.R
+++ R/1-IcapDataPrep.R
@@ -1,7 +1,6 @@
 # Working directory
 setwd("~/Documents/datascience/MITREAP")
 
-
 # Libraries ---------------------------------------------------------------
 # Load and install tidyverse
 if(require(tidyverse) == FALSE){                                                
@@ -10,15 +9,7 @@ if(require(tidyverse) == FALSE){
 }else{                                                                          
   library(tidyverse)                                                            
 }
-                                                     
-# Load and install lubridate
-if(require(lubridate) == FALSE){                                                
-  install.packages('lubridate')                                                 
-  library(lubridate)                                                            
-}else{                                                                          
-  library(lubridate)                                                            
-}
-
+                                             
 # Data --------------------------------------------------------------------
 # Read DENUE from Mexico City
 read_csv("data/raw/denue_inegi_09_.csv", locale = locale(encoding = "ISO-8859-1")) %>% 
@@ -27,33 +18,37 @@ read_csv("data/raw/denue_inegi_09_.csv", locale = locale(encoding = "ISO-8859-1"
     # Remove points and commas
     raz_social = str_remove_all(raz_social, '[\\.,]+'),
     # Extract fiscal registration
-    razon = str_squish(str_extract(raz_social, 'S DE CV| S A P I DE CV| SA$| S[AC] DE CV$| SC?$| SC? [DEN]{2} [RLCS]{2}$| SAPI$| SAPI DE CV$| SC? DE RL DE CV| AC| SPR DE RL|SOFOM| S DE PR DE RL|SA DE RL| AR$|SA DE CV SFC|INC SA DE CV SOFOM ENR|INC|SA DE RL|SAPI DE CV SOFOM ENR| SAB DE CV SOFOM ENR|Y ASOCIADOS|SA DE RL DE CV|SCL|SRL DE CV|SAS DE CV|SC DE R|SRL DE CV|A C|SAS DE CV|S A  DE  C V| SA DE CV SOFOM ENR|SAB DE CV|S DE RL MI|SA DE CV SFP|SA DE CV SOFOM ENR|IAP|API DE CV|SRL DE CV|SA DE CV SOFI|SA DE CV|SAS DE CV')) 
+    razon = str_squish(str_extract(raz_social, 'S DE CV| S A P I DE CV| SA$| S[AC] DE CV$| SC?$| SC? [DEN]{2} [RLCS]{2}$| SAPI$| SAPI DE CV$| SC? DE RL DE CV| AC| SPR DE RL|SOFOM| S DE PR DE RL|SA DE RL| AR$|SA DE CV SFC|INC SA DE CV SOFOM ENR|INC|SA DE RL|SAPI DE CV SOFOM ENR| SAB DE CV SOFOM ENR|Y ASOCIADOS|SA DE RL DE CV|SCL|SRL DE CV|SAS DE CV|SC DE R|SRL DE CV|A C|SAS DE CV|S A  DE  C V| SA DE CV SOFOM ENR|SAB DE CV|S DE RL MI|SA DE CV SFP|SA DE CV SOFOM ENR|IAP|API DE CV|SRL DE CV|SA DE CV SOFI|SA DE CV|SAS DE CV')),
+    razon = str_replace(razon, 'A C$', 'AC'),    
+    razon = str_replace(razon, 'S A P I', 'SAPI'),
+    razon = ifelse(is.na(razon),'Sin Razón', razon)
     ) %>% 
   # Transmute to dataset
   transmute(
     # Business with 30 employes or more(Create dependent variable)
-    employees_30_or_more = if_else(per_ocu %in% c("0 a 5 personas","6 a 10 personas","11 a 30 personas"), 0, 1),
+    employees_30_or_more = factor(
+      if_else(per_ocu %in% c("0 a 5 personas","6 a 10 personas","11 a 30 personas"), 0, 1),
+      levels = 0:1,
+      labels = c('<30 employees', '30 or more employees')
+      ),
     # Business length name
     length_name = nchar(nom_estab),
     # SCIAN Code
-    codigo_act = str_extract(codigo_act, '^\\d{6}'),
+    codigo_act = factor(str_extract(codigo_act, '^\\d{6}')),
     # Road types
-    tipo_vial = factor(tipo_vial, exclude = NULL),
-    tipo_v_e_1 = factor(tipo_vial, exclude = NULL),
-    tipo_v_e_2 = factor(tipo_v_e_2, exclude = NULL),
-    tipo_v_e_3 = factor(tipo_v_e_3, exclude = NULL),
+    tipo_vial = factor(coalesce(tipo_vial,'Sin vialidad')),
     # Has building
-    edificio = factor(!is.na(edificio)),
+    edificio = !is.na(edificio),
     # Where it lies
-    tipo_asent = factor(tipo_asent, exclude = NULL),
+    tipo_asent = factor(coalesce(tipo_asent, 'Sin asentamiento')),
     # Municipality
     municipio = factor(municipio),
     # Has phone
-    telefono = factor(!is.na(telefono)),
+    telefono = !is.na(telefono),
     # Has email
-    correoelec = factor(!is.na(correoelec)),
+    correo_electronico = !is.na(correoelec),
     # Has webpage
-    pagina_web = factor(!is.na(www)),
+    pagina_web = !is.na(www),
     # Type of economic unit
     tipoUniEco = factor(tipoUniEco),
     # Year of kickstart
@@ -62,7 +57,23 @@ read_csv("data/raw/denue_inegi_09_.csv", locale = locale(encoding = "ISO-8859-1"
     month_alta = month(ym(fecha_alta)),
     # Months open
     month_open = as.numeric(ym('2022-11') - ym(fecha_alta))/30,
-    # Fiscal reason
-    razon = factor(razon, exclude = NULL)
+    # Fiscal type dummies
+    razon_sa = str_detect(razon, 'SA'),
+    razon_cv = str_detect(razon, 'CV'),
+    razon_rl = str_detect(razon, 'RL'),
+    razon_ac = str_detect(razon, 'AC'),
+    razon_sapi = str_detect(razon, 'API'),
+    razon_sab = str_detect(razon, 'SAB'),
+    razon_pr = str_detect(razon, 'PR'),
+    razon_sofom = str_detect(razon, 'SOFOM'),
+    razon_sfp = str_detect(razon, 'SFP'),
+    razon_scl = str_detect(razon, 'SCL'),
+    razon_spr = str_detect(razon, 'PR'),
+    razon_sc = str_detect(razon, 'SC'),
+    razon_inc = str_detect(razon, 'INC'),
+    razon_sas = str_detect(razon, 'SAS'),
+    razon_sc = str_detect(razon, 'SC'),
+    razon_sofi = str_detect(razon, 'SOFI'),
   ) %>% 
+  mutate_if(is.logical, as.numeric) %>% 
   write_excel_csv('data/raw/model_input.csv')
--- R/1-IcapModel.R
+++ R/1-IcapModel.R
@@ -17,13 +17,7 @@ if(require(tidymodels) == FALSE){
 }else{                                                                          
   library(tidymodels)                                                            
 }
-# Load and install lubridate
-if(require(lubridate) == FALSE){                                                
-  install.packages('lubridate')                                                 
-  library(lubridate)                                                            
-}else{                                                                          
-  library(lubridate)                                                            
-}
+
 # Load and install sparklyr
 if(require(sparklyr) == FALSE){                                                
   install.packages('sparklyr')                                                 
@@ -42,27 +36,29 @@ sc = spark_connect(master="local", version = '3.3.1')
 # Load data
 denue = spark_read_csv(sc, 'data/raw/model_input.csv', overwrite = TRUE, name = 'denue')
 
-# Feature engeniereing for modeling
+# Feature engineering for modeling
 denue_k = denue %>% 
+  ft_string_indexer(input_col = 'employees_30_or_more', output_col = 'y') %>%
   ft_string_indexer(input_col = 'tipo_vial', output_col = 'tipo_vial_index') %>%
   ft_string_indexer(input_col = 'tipo_asent', output_col = 'tipo_asent_index') %>% 
   ft_string_indexer(input_col = 'municipio', output_col = 'municipio_index') %>% 
   ft_string_indexer(input_col = 'tipoUniEco', output_col = 'tipoUniEco_index') %>% 
   ft_string_indexer(input_col = 'codigo_act', output_col = 'codigo_act_index') %>% 
-  ft_string_indexer(input_col = 'razon', output_col = 'razon_index') %>% 
   ft_one_hot_encoder(
-    input_cols = c('tipo_vial_index','tipo_asent_index','municipio_index','tipoUniEco_index','codigo_act_index','razon_index'),
-    output_cols = c('tipo_vial_onehot','tipo_asent_onehot','municipio_onehot','tipoUniEco_onehot','codigo_act_onehot','razon_onehot')
+    input_cols = c('tipo_vial_index','tipo_asent_index','municipio_index',
+                   'tipoUniEco_index','codigo_act_index'),
+    output_cols = c('tipo_vial_onehot','tipo_asent_onehot','municipio_onehot',
+                    'tipoUniEco_onehot','codigo_act_onehot')
   ) %>% 
   ft_vector_assembler(
-    input_cols =  c('tipo_vial_onehot','tipo_asent_onehot','municipio_onehot','tipoUniEco_onehot','codigo_act_onehot','razon_onehot'), 
+    input_cols =  c('tipo_vial_onehot','tipo_asent_onehot','municipio_onehot','tipoUniEco_onehot','codigo_act_onehot'), 
     output_col = 'one_hot') %>% 
   ft_vector_assembler(
-    input_cols = c('length_name','edificio','telefono','correoelec','year_alta','month_alta','month_open'), 
-    output_col = 'numericas') %>%
-  ft_interaction(input_cols = c('one_hot'), output_col = 'interacciones') %>% 
-  select(employees_30_or_more, numericas, one_hot, interacciones)
-
+    input_cols = c('length_name','edificio','telefono','correo_electronico','year_alta','month_alta','month_open',
+                   "razon_cv","razon_rl","razon_ac","razon_sapi","razon_sab","razon_pr","razon_sofom",
+                   "razon_sfp","razon_scl","razon_spr", "razon_sc","razon_inc","razon_sas","razon_sofi"), 
+    output_col = 'numericas') 
+  
 
 # Dataset in unbalanced
 pull(denue, employees_30_or_more) %>% 
@@ -72,10 +68,10 @@ pull(denue, employees_30_or_more) %>%
 # Tuning ------------------------------------------------------------------
 # Balancing the dataset
 balanceo_true = denue_k  %>% 
-  filter(employees_30_or_more == 1)
+  filter(y == 1)
 
 balanceo_false = denue_k  %>% 
-  filter(employees_30_or_more == 0) %>% 
+  filter(y == 0) %>% 
   sample_n(size = 17174)
 
 balanceo = sdf_bind_rows(balanceo_true, balanceo_false)
@@ -92,14 +88,14 @@ test = datos_split %>%
 
 # Create a pipeline
 pipeline = ml_pipeline(sc) %>%
-  ft_r_formula(employees_30_or_more ~ numericas + one_hot + interacciones) %>%
+  ft_r_formula(y ~ numericas + one_hot) %>%
   ml_logistic_regression()
 
 # Tuning grid
 grid =  list(
   logistic_regression = list(
     elastic_net_param = 1, 
-    reg_param = seq(0, 10, length = 100)    
+    reg_param = seq(0, 1, length = 100)    
   )
 )
 
@@ -135,32 +131,52 @@ reg_param = cv_model %>%
 # Fit the best model
 iq_model = logistic_reg(penalty = reg_param) %>% 
   set_mode('classification')%>% 
-  set_engine("spark") %>%
-  fit(
-    formula = employees_30_or_more ~ numericas + one_hot + interacciones,
+  set_engine("spark") 
+
+iq_fit = iq_model %>% fit(
+    formula = y ~ numericas + one_hot,
     data = training
       )
 
 # Check training performance
-iq_model %>% 
+iq_fit %>% 
   augment(training) %>% 
-  select(employees_30_or_more, .prediction) %>% 
+  select(y, .prediction) %>% 
   collect() %>% 
   mutate_all(as.factor) %>% 
-  conf_mat(truth = employees_30_or_more, estimate = .prediction) %>% 
+  conf_mat(truth = y, estimate = .prediction) %>% 
   summary()
 
 # Check test performance
-iq_model %>% 
+iq_fit %>% 
   augment(test) %>% 
-  select(employees_30_or_more, .prediction) %>% 
+  select(y, .prediction) %>% 
   collect() %>% 
   mutate_all(as.factor) %>% 
-  conf_mat(truth = employees_30_or_more, estimate = .prediction) %>% 
+  conf_mat(truth = y, estimate = .prediction) %>% 
   summary()
 
+# Check full dataset performance
+iq_fit %>% 
+  augment(denue_k) %>% 
+  select(y, .prediction) %>% 
+  collect() %>% 
+  mutate_all(as.factor) %>% 
+  conf_mat(truth = y, estimate = .prediction) %>% 
+  summary()
+
+# Predict probabilities (IQ)
+results = iq_fit %>% 
+  extract_fit_engine() %>% 
+  ml_predict(denue_k) %>% 
+  select(employees_30_or_more:razon_sofi, prediction:probability_1) %>% 
+  collect() 
+
+glimpse(results)
+write_excel_csv(results, 'data/outputs/denue_iq_results.csv')
+
 # Tidying coeficients
-coefficients = tidy(iq_model) 
+coefficients = tidy(iq_fit) 
 write_excel_csv(coefficients, 'data/raw/coefficients.csv')
 
 # We are done with spark
